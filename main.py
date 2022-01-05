204a996ce5a940fe8277374c5415295df6cb6a5c
John Li <cloudsunning@vip.qq.com>
2022-01-05 16:25:39 +0800

    利用nltk分词器处理pdf文档

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # This is the first edition about cope with nlp
-import nltk
-print(dir(nltk))
+from nltk.tokenize import WordPunctTokenizer
+from nltk.tokenize import RegexpTokenizer
+from PyPDF2 import PdfFileReader as pdf_read
+
+# 输入一个句子，分成词组
+def wordtokenizer(sentence):
+        #分段
+    words = WordPunctTokenizer().tokenize(sentence)
+    return words
+
+# 处理PDF文档, 其中a为文件具体位置，如：x:\xxx\xxx.pdf
+with open(a, 'rb') as f:
+#     根据正则初始化分词器
+    tokenizer = RegexpTokenizer(r'[a-zA-Z]\w+')
+    pdf = pdf_read(f)
+    con_txt = pdf.getPage(10).extractText()
+    print(con_txt)
+    l2 = con_txt.split("\n")
+    print(len(l2))
